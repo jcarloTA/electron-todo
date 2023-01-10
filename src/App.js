a3f93156f4f3b34113570a9e32bd071ae3bc31c4
Jancarlo Ticún <janta1696@gmail.com>
2023-01-10 00:03:54 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import LocalStorageHelper from './utils/helpers/local-storage-helper/local-storage-helper';
+import TodoList from './components/todo-list/todo-list';
+import TodoForm from './components/todo-form/todo-form';
+import { Container, Row, Col } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+
+
+class Home extends React.Component {
+  state = {
+    todos: LocalStorageHelper.getTodos()
+  }
+
+  addTodo = (todo) => {
+    this.setState(previousState => ({
+      todos: [...previousState.todos, todo]
+    }));
+  }
+
+  removeTodo = (id) => {
+    LocalStorageHelper.removeTodo(id);
+    let todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+
+    this.setState({
+      todos
+    });
+  }
+
+  render() {
+    const {todos} = this.state;
+
+    return (
+      <Container className="App">
+        <Row>
+          <Col>
+            <h1>Todo List</h1>
+          </Col>
+        </Row>
+        <TodoForm addTodo={this.addTodo}/>
+        <TodoList todos={todos} removeTodo={this.removeTodo} />
+      </Container>
+    );
+  }
 }
 
-export default App;
+export default Home
